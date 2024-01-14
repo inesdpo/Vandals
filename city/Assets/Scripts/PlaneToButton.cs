dad1ea338c2a7e92d6d538eb58889b0033ac7377
inesdpo <117300107+inesdpo@users.noreply.github.com>
2024-01-14 04:52:31 +0000

    script and camera correction

--- city/Assets/Scripts/PlaneToButton.cs
+++ city/Assets/Scripts/PlaneToButton.cs
@@ -4,30 +4,14 @@ using UnityEngine;
 
 public class PlaneToButton : MonoBehaviour
 {
-    /*private void Update()
-    {
-        Vector3 planeposition = transform.position;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if ((Physics.Raycast(ray, 100)) && (Input.GetMouseButtonDown(0)))
-        {
-            planeposition = ray.origin; 
-            Debug.Log("Hit something");
-            GetComponent<PlaneToButton>().OnMouseDown();
-        }
-    }*/
-    /*void OnMouseOver()
-    {
-        Vector3 planeposition = transform.position;
-    }*/
-
-
     [SerializeField] private GameObject objectToDeactivate;
     [SerializeField] private GameObject objectToActivate;
-    void OnMouseDown()
+
+    public void OnMouseDown()
     {
         objectToDeactivate.SetActive(false);
         objectToActivate.SetActive(true);
-        Debug.Log("Touched something");
+        
     }
 }
 
